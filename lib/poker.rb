92e18947efc54d451aba2e7e40f90b5834c9a4c2
Andrew Nordman <andrew.nordman@kehe.com>
2010-09-17 16:55:40 -0500

    Removed deleted file from requires

--- lib/poker.rb
+++ lib/poker.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/ratio'
+require File.dirname(__FILE__) + '/poker/deck'
+require File.dirname(__FILE__) + '/poker/game'
+require File.dirname(__FILE__) + '/poker/scenario'
+require File.dirname(__FILE__) + '/poker/result'
+require File.dirname(__FILE__) + '/poker/player'
+require File.dirname(__FILE__) + '/poker/hand_range'
 
 module Poker
-  require File.dirname(__FILE__) + '/poker/card'
-  require File.dirname(__FILE__) + '/poker/deck'
-  require File.dirname(__FILE__) + '/poker/game'
-  require File.dirname(__FILE__) + '/poker/scenario'
-  require File.dirname(__FILE__) + '/poker/result'
-  require File.dirname(__FILE__) + '/poker/player'
-  require File.dirname(__FILE__) + '/poker/hand_range'
+  # stub
 end
