33932e064260bd025b7a872f8a29084b0f54c1e6
Andrew Nordman <andrew.nordman@kehe.com>
2010-09-21 19:42:14 -0500

    Reversed card order

--- lib/poker/deck.rb
+++ lib/poker/deck.rb
@@ -1,6 +1,6 @@
 module Poker
   class Deck
-    CARDS = %w[As Ks Qs Js Ts 9s 8s 7s 6s 5s 4s 3s 2s Ah Kh Qh Jh Th 9h 8h 7h 6h 5h 4h 3h 2h Ad Kd Qd Jd Td 9d 8d 7d 6d 5d 4d 3d 2d Ac Kc Qc Jc Tc 9c 8c 7c 6c 5c 4c 3c 2c]
+    CARDS = %w[2c 3c 4c 5c 6c 7c 8c 9c Tc Jc Qc Kc cA 2d 3d 4d 5d 6d 7d 8d 9d Td Jd Qd Kd dA 2h 3h 4h 5h 6h 7h 8h 9h Th Jh Qh Kh hA 2s 3s 4s 5s 6s 7s 8s 9s Ts Js Qs Ks sA]
   
     def initialize
       @dealt_cards = 0
